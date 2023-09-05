447ce58c676ac44bb7ee4d1e876d774f8286fee0
nikita_kirillov <Kirillov.Niikita@yandex.ru>
2023-09-05 08:36:29 +0600

    You can write everything on one line.

--- collectionslist/src/Main.java
+++ collectionslist/src/Main.java
@@ -1,32 +1,60 @@
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
     private static TodoList todoList = new TodoList();
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int index = 0;
+        String regexCommand = "[A-Z]+";
+        String regexIndex = "[0-9]+";
+        String regexText = "[\\s,a-z]+";
+        String command = "";
+        String todo = null;
         while (true) {
             String input = scanner.nextLine();
-            if (input.equals("ADD")) {
-                System.out.println("Введите задачу: ");
-                input = scanner.nextLine();
-                todoList.add(input);
-                System.out.println(todoList.getTodos().size() + " - кол-во задач");
-            } else if (input.equals("DELETE")) {
-                System.out.println("Введите интекс задачи: ");
-                index = scanner.nextInt();
+            Pattern pCommand = Pattern.compile(regexCommand);
+            Matcher mCommand = pCommand.matcher(input);
+            Pattern pIndex = Pattern.compile(regexIndex);
+            Matcher mIndex = pIndex.matcher(input);
+            while (mCommand.find()) {
+                int start = mCommand.start();
+                int end = mCommand.end();
+                command = input.substring(start, end);
+            }
+            while (mIndex.find()) {
+                int start = mIndex.start();
+                int end = mIndex.end();
+                index = Integer.parseInt(input.substring(start, end));
+            }
+            Pattern pText = Pattern.compile(regexText);
+            Matcher mText = pText.matcher(input);
+            while (mText.find()) {
+                int start = mText.start();
+                int end = mText.end();
+                todo = input.substring(start + 1, end);
+            }
+            if (command.equals("ADD")) {
+                if (todo.isEmpty()){
+                    System.out.println("Ошибка: введите задачу латинскими буквами");
+                } else {
+                    todoList.add(todo);
+                    System.out.println(todoList.getTodos().size() + " - кол-во задач");
+                }
+            } else if (command.equals("DELETE")) {
                 todoList.delete(index - 1);
-                System.out.println("Список задач:\n" + todoList.getTodos().size() + " - кол-во задач");
-            } else if (input.equals("EDIT")) {
-                System.out.println("Введите новую задачу: ");
-                input = scanner.nextLine();
-                System.out.println("Введите интекс задачи: ");
-                index = scanner.nextInt();
-                System.out.println("Задачи обновлены: ");
-                todoList.edit(index - 1, input);
                 System.out.println(todoList.getTodos().size() + " - кол-во задач");
-            } else if (input.equals("LIST")) {
-                System.out.println(todoList.getTodos().size() + " - кол-во задач");
+            } else if (command.equals("EDIT")) {
+                if (todo.isEmpty()){
+                    System.out.println("Ошибка: введите задачу латинскими буквами");
+                } else {
+                    todoList.edit(index - 1,todo);
+                    System.out.println(todoList.getTodos().size() + " - кол-во задач");
+                }
+            } else if (command.equals("LIST")) {
+                todoList.getTodos();
             } else if (input.equals("HELP")) {
                 System.out.println(" LIST — выводит дела с их порядковыми номерами." +
                         "\n ADD — добавляет дело в конец списка или дело на определённое место." +
@@ -38,6 +66,7 @@ public class Main {
             } else {
                 System.out.println("Ошибка ввода: введите \"HELP\" для списка команд");
             }
+
         }
         // TODO: написать консольное приложение для работы со списком дел todoList
     }
